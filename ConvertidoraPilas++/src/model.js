a1b2f228f630454820536ffb44c33ed6d07af334
Rodrigo Vz Rys (Roy) <152117781+TRoyGms@users.noreply.github.com>
2024-03-22 03:37:48 -0600

    [UPLOAD] Firts Upload

---
+++ ConvertidoraPilas++/src/model.js
@@ -0,0 +1,13 @@
+export class Model {
+    constructor() {
+        this.expression = '';
+    }
+
+    setExpression(expression) {
+        this.expression = expression;
+    }
+
+    getExpression() {
+        return this.expression;
+    }
+}
